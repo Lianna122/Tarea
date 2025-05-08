8b4d5c1046b9a2e98d967c4bf21642783c8b3ae1
Lianna122 <71226137@idexpasco.edu.pe>
2025-05-07 21:07:45 -0500

    El java

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ document.addEventListener('DOMContentLoaded', function() {
             const nombreProducto = this.dataset.nombre;
             const precioProducto = this.dataset.precio;
             alert(`¡Has añadido ${nombreProducto} al carrito por $${precioProducto}! (Esta funcionalidad es solo un ejemplo)`);
-            // Aquí podrías agregar la lógica real para añadir al carrito
         });
     });
 });
